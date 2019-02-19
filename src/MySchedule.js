b87df1a7b8770809c1a74893ac0b469ac6ecbb8c
Tieu Luu <tieu@qmulos.com>
2019-02-18 21:13:16 -0500

    round 2 trying to get showing enrolled classes to work

--- src/MySchedule.js
+++ src/MySchedule.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import {db} from "./firebase";
+import {auth, db} from "./firebase";
 
 class MySchedule extends Component {
+
+  refreshItems() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.refreshItems();
+
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  }
+
   render() {
     if (this.props.getUser()) {
       var ref = db.collection("student-classes");
       var query = ref.where("username", "==", this.props.getUser().user.email);
       console.log("about to execute query");
-      query.get().then(function(results) {
+      var enrolledClasses;
+      query.get().then((results) => {
         if(results.empty) {
           console.log("No documents found!");
           return (
@@ -17,29 +47,30 @@ class MySchedule extends Component {
           );
         }
         else {
-          return (
-            <React.Fragment>
-              {
-                results.forEach(function (doc) {
-                  console.log("Document data:", doc.data().class_id + " " + doc.data().username);
-                  return (
-                    console.log("HEHE"),
-                    <p>
-                      {doc.data().class_id}
-                    </p>
-                  )
-                })
-              }
-            </React.Fragment>
-          );
+          console.log("DOCS: " + results.size + results.docs);
+          //console.log("Document data:", doc.data().class_id + " " + doc.data().username);
+          enrolledClasses = results.docs.map((doc) => {
+            console.log("Class: " + doc.data().class_id);
+            return (
+              <p>
+                {doc.data().class_id}
+              </p>
+            );
+          });
+          //console.log("ENROLLED CLASSES: " + enrolledClasses);
         }
-      }).catch(function(error) {
-        console.log("Error getting documents:", error);
-        return null;
       });
+      console.log("ENROLLED CLASSES: " + enrolledClasses);
+      return (
+        <React.Fragment>
+        <h1>Enrolled Classes</h1>
+        <div>{enrolledClasses}</div>
+        </React.Fragment>
+      );
     }
     else {
-      return null;
+      console.log("Not logged in");
+      return (<div/>);
     }
   }
 }
